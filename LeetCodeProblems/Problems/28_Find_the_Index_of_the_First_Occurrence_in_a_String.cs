42e82a9a67f24d39539de544a477b3b5520cebfb
Sbaroi Ionut-Alexandru <sbaroialex@gmail.com>
2024-01-13 11:59:24 +0200

    - added new problem

---
+++ LeetCodeProblems/Problems/28_Find_the_Index_of_the_First_Occurrence_in_a_String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems._28;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int index = haystack.IndexOf(needle);
+
+        if (index != -1)
+        {
+            return index;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
