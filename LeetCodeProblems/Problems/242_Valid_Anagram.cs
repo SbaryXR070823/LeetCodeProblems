370c70c53c5e8bded35b6aa04c8bc5f8c625cbe9
Sbaroi Ionut-Alexandru <sbaroialex@gmail.com>
2024-01-13 12:17:37 +0200

    - added new problem

---
+++ LeetCodeProblems/Problems/242_Valid_Anagram.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems._242;
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        Dictionary<char, int> sMap = new Dictionary<char, int>();
+        Dictionary<char, int> tMap = new Dictionary<char, int>();
+        for (int i=0; i<s.Length; i++)
+        {
+            if (sMap.ContainsKey(s[i]))
+            {
+                sMap[s[i]]++;
+            }else
+            {
+                sMap.Add(s[i], 1);
+            }
+
+            if (tMap.ContainsKey(t[i]))
+            {
+                tMap[t[i]]++;
+            }
+            else
+            {
+                tMap.Add(t[i], 1);
+            }
+        }
+
+        foreach (var kvp in sMap)
+        {
+            if (tMap.ContainsKey(kvp.Key))
+            {
+                if (tMap[kvp.Key] != kvp.Value)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
